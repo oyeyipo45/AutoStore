9496c4cf8bfa99faa500dc96a7434ca24b83f949
oyeyipo45 <oyeyipo45@gmail.com>
2020-11-02 21:56:54 +0100

    server created

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+
+const app = express()
+
+
+
+app.listen(5001, console.log("app running on PORT 5000"))
--- frontEnd/src/components/Header.js
+++ frontEnd/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,12 +7,23 @@ const Header = () => {
 		<header>
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
-					<Navbar.Brand href='/'>AUTO Store</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand>AUTO Store</Navbar.Brand>
+					</LinkContainer>
+
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ml-auto'>
-							<Nav.Link href='/cart'>Cart <i className="fas fa-shopping-cart"></i>  </Nav.Link>
-							<Nav.Link href='/login'>Sign In <i className="fas fa-user"></i></Nav.Link>
+							<LinkContainer to='/cart'>
+								<Nav.Link>
+									Cart <i className='fas fa-shopping-cart'></i>{' '}
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link>
+									Sign In <i className='fas fa-user'></i>
+								</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
