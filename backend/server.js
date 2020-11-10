4c5ebab0bee332e23895299fba386ed4472509fd
oyeyipo45 <oyeyipo45@gmail.com>
2020-11-10 06:39:23 +0100

    add and remove from cart functional

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,7 @@ import connectDb from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js'
 import {notFound, errorHandler } from './middleware/errorMiddlerware.js'
-
-
+import morgan from 'morgan'
 
 
 
@@ -24,7 +23,7 @@ app.use(`/api/products`, productRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
-
+app.use(morgan('tiny'))
 
 const PORT = process.env.PORT || 5001;
 
