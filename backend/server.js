a6dc4bb72f5495aa99fcc15f2ff5535ef897b249
oyeyipo45 <oyeyipo45@gmail.com>
2020-11-21 21:59:57 +0100

    heroku

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,60 @@
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDb from './config/db.js';
-import colors from 'colors';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js';
-import productRoutes from './routes/productRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddlerware.js';
-import morgan from 'morgan';
+import path from 'path'
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import morgan from 'morgan'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from './config/db.js'
 
-dotenv.config();
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
-connectDb();
+dotenv.config()
 
-const app = express();
+connectDB()
+
+const app = express()
 
 if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+  app.use(morgan('dev'))
 }
-app.use(express.json());
 
-app.use(`/api/v1/products`, productRoutes);
-app.use(`/api/v1/users`, userRoutes);
-app.use(`/api/v1/orders`, orderRoutes);
-app.use('/api/v1/upload', uploadRoutes);
-app.get('/api/v1/config/paypal', (req, res) =>
-	res.send(process.env.PAYPAL_CLIENT_ID)
-);
+app.use(express.json())
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-	app.get('*', (req, res) =>
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-	);
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
 } else {
-	app.get('/', (req, res) => {
-		res.send('server is running');
-	});
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
-app.use(notFound);
-app.use(errorHandler);
+app.use(notFound)
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000
 
 app.listen(
-	PORT,
-	console.log(
-		`app running in  ${process.env.NODE_ENV} mode  on PORT ${PORT}`.yellow.bold
-	)
-);
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+)
